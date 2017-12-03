f43c27c5b0315b77f718bbe8e4dbed40abf12cdc
Damien8x <damien.sudol@gmail.com>
2017-12-03 12:55:06 -0800

    Add files via upload

---
+++ FOLLOWERS.php
@@ -0,0 +1,36 @@
+<?php
+$servername = “cssql.seattleu.edu";
+$username = "username";
+$password = "password";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+mysqli_close($conn);
+
+$sql = "SELECT * FROM FOLLOWERS";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+echo "<table border = '1'>\n";
+// output data of each row
+echo "<tr>\n";
+echo "<th>Course Code</th>\n<th>Course
+Name</th>\n";
+echo "</tr>\n";
+while($row = mysqli_fetch_row($result)) {
+echo "<tr>\n";
+echo "<td>" . $row[0] . "</td>\n" . "<td>".
+$row[1] . "</td>\n";
+echo "</tr>\n";
+}
+echo "</table>\n";
+} else {
+echo "0 results";
+}
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
+
